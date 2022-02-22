f57ba7cfb7f7175ff9a600d30cd909e27b90cf99
Saranjith p <saranvlmna@gmail.com>
2022-02-22 12:39:48 +0530

    sum

---
+++ JAVA/sum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class sum {
+    public static void main(String[] args) {
+        // input 
+        Scanner s=new Scanner(System.in);
+        //print
+        System.out.println("Enter Tow Numbers");
+        //onput value assin
+        int a=s.nextInt();
+        int b=s.nextInt();
+        //sum
+        int result=a+b;
+        //print result
+        System.out.println("Sum of Two Numbers is:"+result);
+    }
+    
+}
