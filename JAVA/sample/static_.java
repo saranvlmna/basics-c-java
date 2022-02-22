577928458f7b1571c4cfec272490a7f9dfd2ef8f
shyamvlmna <shyamvlmna@gmail.com>
2022-02-22 21:48:55 +0530

    static

---
+++ JAVA/sample/static_.java
@@ -0,0 +1,18 @@
+
+public class static_ {
+
+    // int a=5;     Error only access static method or static variable in a static method 
+    public static void main(String[] args) {
+        // System.out.println(""+a);
+        // System.out.println(""+hi);
+     sample();
+    }
+
+    // void sample(){    Error only access static method or static variable in a static method 
+    //     // System.out.println("hi");
+    // }
+
+    static void sample(){
+      System.out.println("hi");
+    }
+}
--- JAVA/sample/sum.java
+++ JAVA/sample/sum.java
@@ -1,4 +1,3 @@
-package sample;
 
 import java.util.Scanner;
 public class sum {
