319a6ed98f035da41f13f60fa7a9e9df6d2b35b6
shyamvlmna <shyamvlmna@gmail.com>
2022-02-23 20:19:04 +0530

    Overload_OverRide

--- JAVA/Polymophism/methodOverRiding.java
+++ JAVA/Polymophism/methodOverRiding.java
@@ -1,3 +1,8 @@
 public class methodOverRiding {
-    
+    public static void main(String[] args) {
+        // create object base class
+        OverRide_Second s=new OverRide_Second();
+        //only print sub. class hide second
+        s.display();
+    }
 }
