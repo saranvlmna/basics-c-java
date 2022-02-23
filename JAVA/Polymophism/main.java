903ec831903ae25359e80e32a9da4131fe930c76
Saranjith p <saranvlmna@gmail.com>
2022-02-23 19:35:58 +0530

    bug fix

---
+++ JAVA/Polymophism/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        methodOverloading s = new methodOverloading();
+        // call function without argumant
+        s.display();
+        // call function with argument
+        s.display(10);
+    }
+}
---
+++ JAVA/Polymophism/methodOverloadint.java
@@ -0,0 +1,13 @@
+package polymophism;
+
+public class methodOverloading {
+    void display() {
+        System.out.println("hello");
+    }
+
+    void display(int a) {
+        System.out.println("hi" + a);
+    }
+
+    //decalre same name function but one function paramerter or type
+}
