ad6ec3e3715548bf05c5e8814332fab8caf3672b
Saranjith p <saranvlmna@gmail.com>
2022-02-25 00:19:24 +0530

    bug fix

--- JAVA/Classs_Object/Abstract/Main_abstract.java
+++ JAVA/Classs_Object/Abstract/Main_abstract.java
@@ -1,4 +1,6 @@
 public abstract class Main_abstract{
 //call a abstract function with object
+//abstract class support fuction delaration and defanition
+// interface only function declaration call implements
     abstract void onText(String text);
 }
